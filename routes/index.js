30e8b6814a9824b3f546f4275dad8824dddb2ca0
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-02-19 22:38:25 -0500

    added expressJS support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ let router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Home page" });
 });
-
+/* GET about page. */
+router.get("/about", function(req, res, next) {
+  res.render("index", { title: "About page" });
+});
+/* GET product page. */
+router.get("/product", function(req, res, next) {
+  res.render("index", { title: "Product page" });
+});
+/* GET services page. */
+router.get("/services", function(req, res, next) {
+  res.render("index", { title: "Services page" });
+});
+/* GET contact page. */
+router.get("/contact", function(req, res, next) {
+  res.render("index", { title: "Contact Page" });
+});
 module.exports = router;
